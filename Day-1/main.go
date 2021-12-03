5cff783c33a801d07316d8f41f908c116d09e847
OJPARKINSON <olliepar01@gmail.com>
2021-12-03 00:10:16 +0000

    day 1 and 2 completed

---
+++ Day-1/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer file.Close()
+
+	var lines []int
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		i, _ := strconv.Atoi(scanner.Text())
+		lines = append(lines, i)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	counter := 0
+
+	for i := 0; i < len(lines)-3; i++ {
+		value := lines[i] + lines[i+1] + lines[i+2]
+		value2 := lines[i+1] + lines[i+2] + lines[i+3]
+		if value < value2 {
+			counter++
+		}
+	}
+
+	fmt.Println(counter)
+
+}
---
+++ Day-2/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var horizontalPosition int = 0
+	var depth int = 0
+	var aim int = 0
+
+	file, err := os.Open("input")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	for i := 0; i < len(lines); i++ {
+		direction := strings.Split(lines[i], " ")
+		directionAmount, _ := strconv.Atoi(direction[1])
+		switch direction[0] {
+		case "forward":
+			horizontalPosition += directionAmount
+			depth += aim * directionAmount
+		case "up":
+			aim -= directionAmount
+		case "down":
+			aim += directionAmount
+		}
+	}
+
+	fmt.Println(horizontalPosition * depth)
+}
