6b1dd641327aabf91a3da4e50a553041b5f88cf2
OJPARKINSON <olliepar01@gmail.com>
2025-12-09 23:21:08 +0100

    day06 part2

--- 2025/day06/main.go
+++ 2025/day06/main.go
@@ -6,12 +6,15 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 func main() {
 	input := readInput()
 
-	fmt.Println(part1(input))
+	fmt.Println("part1: ", part1(input))
+	fmt.Println("part2: ", part2(input))
+
 }
 
 func part1(problems [][]string) int {
@@ -45,8 +48,88 @@ func part1(problems [][]string) int {
 			}
 		}
 		grandTotal = grandTotal + colTotal
-		fmt.Println("---")
 	}
+	return grandTotal
+}
+
+func part2(problems [][]string) int {
+	file, _ := os.Open("./input.txt")
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if len(lines) == 0 {
+		return 0
+	}
+
+	maxLen := 0
+	for _, line := range lines {
+		if len(line) > maxLen {
+			maxLen = len(line)
+		}
+	}
+
+	grid := make([][]rune, len(lines))
+	for i, line := range lines {
+		runes := []rune(line)
+		for len(runes) < maxLen {
+			runes = append(runes, ' ')
+		}
+		grid[i] = runes
+	}
+
+	grandTotal := 0
+	var currentNums []int
+	currentOp := ""
+
+	for col := len(grid[0]) - 1; col >= 0; col-- {
+		numStr := ""
+		for row := 0; row < len(grid); row++ {
+			char := grid[row][col]
+
+			if unicode.IsDigit(char) {
+				numStr += string(char)
+			} else if char == '*' || char == '+' {
+				currentOp = string(char)
+			}
+		}
+
+		if numStr != "" {
+			number, _ := strconv.Atoi(numStr)
+			currentNums = append(currentNums, number)
+		} else if len(currentNums) > 0 {
+			result := currentNums[0]
+			for i := 1; i < len(currentNums); i++ {
+				if currentOp == "*" {
+					result *= currentNums[i]
+				} else {
+					result += currentNums[i]
+				}
+			}
+			fmt.Println("Problem:", currentNums, currentOp, "=", result)
+			grandTotal += result
+			currentNums = nil
+			currentOp = ""
+		}
+	}
+
+	if len(currentNums) > 0 {
+		result := currentNums[0]
+		for i := 1; i < len(currentNums); i++ {
+			if currentOp == "*" {
+				result *= currentNums[i]
+			} else {
+				result += currentNums[i]
+			}
+		}
+		fmt.Println("Problem:", currentNums, currentOp, "=", result)
+		grandTotal += result
+	}
+
 	return grandTotal
 }
 
